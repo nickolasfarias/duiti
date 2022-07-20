f4d7be445e3de9e7eb71ce54c2ce5e11c228f393
Nickolas Farias <nickolasfarias@gmail.com>
2022-07-19 21:40:39 -0300

    a lot

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,6 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  def index
+    @pagy, @records = pagy(Category.all, items: 8)
+  end
 end
